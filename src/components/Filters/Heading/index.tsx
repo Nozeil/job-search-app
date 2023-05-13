d5e1612fa28a0a6e2c685d0c0b7504ee69a51ec8
Artur Voloshin <nozeilwrk@gmail.com>
2023-05-13 17:55:31 +0300

    feat: add border to filters

--- src/components/Filters/Heading/index.tsx
+++ src/components/Filters/Heading/index.tsx
@@ -3,7 +3,7 @@ import ResetButton from './ResetButton';
 
 export default function Heading() {
   return (
-    <Group spacing={67}>
+    <Group spacing={66}>
       <Title size={rem(20)} fw="bold" lh={1}>
         Фильтры
       </Title>
--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,10 +1,16 @@
-import { Stack } from '@mantine/core';
+import { MantineTheme, Stack } from '@mantine/core';
 import Heading from './Heading';
 import Controls from './Controls';
+import { COLORS_KEYS } from '@/constants';
+
+const sx = (theme: MantineTheme) => ({
+  border: `1px solid ${theme.colors[COLORS_KEYS.GREY_SCALE][2]}`,
+  borderRadius: 8,
+});
 
 export default function Filters() {
   return (
-    <Stack spacing={32} p="lg" maw={315}>
+    <Stack spacing={32} p="lg" maw={315} sx={sx}>
       <Heading />
       <Controls />
     </Stack>
