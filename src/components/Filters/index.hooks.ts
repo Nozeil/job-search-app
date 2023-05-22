27b03abe8b32ee3ed4ca6c495a7acb0bb9af6b7b
Nozeil <nozeilwrk@gmail.com>
2023-05-22 22:06:48 +0300

    fix: return background color to filters

--- src/components/Filters/index.hooks.ts
+++ src/components/Filters/index.hooks.ts
@@ -3,6 +3,7 @@ import { createStyles } from '@mantine/core';
 export const useStyles = createStyles((theme) => ({
   container: {
     maxWidth: 315,
+    background: theme.white,
 
     [theme.fn.smallerThan('sm')]: {
       maxWidth: 'none',
