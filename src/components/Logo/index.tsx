86e63bcc1204d4480f7740e422bf4d30669c86de
Artur Voloshin <nozeilwrk@gmail.com>
2023-05-22 11:38:38 +0300

    feat: change page favicon and title

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,10 @@
 import { FONTS } from '@/constants';
 import { Group, Title, Image, rem } from '@mantine/core';
-import logo from '@/assets/logo.svg';
 
 export default function Logo() {
   return (
     <Group pos="absolute" top="50%" spacing={12} sx={{ transform: 'translate(0, -50%)' }}>
-      <Image width={30} height={30} src={logo} alt="logo" />
+      <Image width={30} height={30} src="/logo.svg" alt="logo" />
       <Title size={rem(24)} ff={FONTS.PRIMARY} fw={600} lts={-0.02}>
         Jobored
       </Title>
